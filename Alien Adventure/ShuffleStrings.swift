e95ccda215f4ca034bb1a76360649c53d2f52460
Emanuele Bartolomucci <e@emanuelebartolomucci.com>
2016-08-15 09:54:07 +0100

    ShuffleString

--- Alien Adventure/ShuffleStrings.swift
+++ Alien Adventure/ShuffleStrings.swift
@@ -2,13 +2,41 @@
 //  ShuffleStrings.swift
 //  Alien Adventure
 //
-//  Created by Jarrod Parkes on 9/30/15.
-//  Copyright © 2015 Udacity. All rights reserved.
+//  Edited by Emanuele Bartolomucci on 2016/08/14.
 //
 
 extension Hero {
     
     func shuffleStrings(s1 s1: String, s2: String, shuffle: String) -> Bool {
+        
+        if s1.isEmpty && s2.isEmpty && shuffle.isEmpty {
+            return true
+        }
+        
+        if shuffle.characters.count != (s1.characters.count + s2.characters.count) {
+            return false
+        }
+        
+        var s1index = s1.startIndex
+        var s2index = s2.startIndex
+        
+        for item in shuffle.characters {
+            
+            // compares the current char in shuffle with the one in either s1 or s2 at the current index, starting from 0
+            
+            if s1index < s1.endIndex && item == s1[s1index] {
+                    s1index = s1index.successor()
+                }
+                
+             else if s2index < s2.endIndex && item == s2[s2index]  {
+                    s2index = s2index.successor()
+                }
+                
+             else {
+                return false
+            }
+        }
+        
         return true
     }
 }
